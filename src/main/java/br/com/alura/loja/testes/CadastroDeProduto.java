9027aff78e9364b101c323e081fabce00df499c5
José Bezerra <josbezfi@gmail.com>
2021-09-14 07:46:19 -0300

    Cadastrando, atualizando e removendo produtos.

--- src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
+++ src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -1,33 +1,32 @@
 package br.com.alura.loja.testes;
 
-import br.com.alura.loja.dao.CategoriaDAO;
-import br.com.alura.loja.dao.ProdutoDAO;
 import br.com.alura.loja.modelo.Categoria;
-import br.com.alura.loja.modelo.Produto;
 import br.com.alura.loja.util.JPAUtil;
 
 import javax.persistence.EntityManager;
-import java.math.BigDecimal;
 
 public class CadastroDeProduto {
 
     public static void main(String[] args) {
         Categoria celulares = new Categoria("CELULARES");
-        Produto celular = new Produto("Iphone 11", "Red",
-                new BigDecimal("800"), celulares);
-
 
         EntityManager em = JPAUtil.getEntityManager();//compartilhado com as DAOS
-        ProdutoDAO produtoDAO = new ProdutoDAO(em);
-        CategoriaDAO categoriaDAO = new CategoriaDAO(em);
 
         em.getTransaction().begin();
 
-        categoriaDAO.cadastrar(celulares);
-        produtoDAO.cadastrar(celular);
+        em.persist(celulares);
+        celulares.setNome("Notebooks");
 
-        em.getTransaction().commit();
-        em.close();
+        em.flush();//sincroniza parcialmente com o banco
+        em.clear();//limpa as entidades gerenciadas
+
+        celulares = em.merge(celulares);//devolve nova referência
+        celulares.setNome("Desktop");
+        em.flush();//continua managed
+        em.clear();
+        em.remove(celulares);
+        em.flush();
+
 
     }
 }
