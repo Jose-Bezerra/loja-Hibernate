9be0d05041260639495d399f372ee3c049f4f9be
José Bezerra <josbezfi@gmail.com>
2021-09-13 21:35:32 -0300

    Simulando o ciclo de vida de uma entidade.

--- src/main/java/br/com/alura/loja/testes/CicloDeVidaEntidade.java
+++ src/main/java/br/com/alura/loja/testes/CicloDeVidaEntidade.java
@@ -9,25 +9,21 @@ import br.com.alura.loja.util.JPAUtil;
 import javax.persistence.EntityManager;
 import java.math.BigDecimal;
 
-public class CadastroDeProduto {
+public class CicloDeVidaEntidade {
 
     public static void main(String[] args) {
         Categoria celulares = new Categoria("CELULARES");
-        Produto celular = new Produto("Iphone 11", "Red",
-                new BigDecimal("800"), celulares);
-
 
         EntityManager em = JPAUtil.getEntityManager();//compartilhado com as DAOS
-        ProdutoDAO produtoDAO = new ProdutoDAO(em);
-        CategoriaDAO categoriaDAO = new CategoriaDAO(em);
 
         em.getTransaction().begin();
 
-        categoriaDAO.cadastrar(celulares);
-        produtoDAO.cadastrar(celular);
+        em.persist(celulares);
+        celulares.setNome("Notebooks");
 
         em.getTransaction().commit();
         em.close();
+
 
     }
 }
