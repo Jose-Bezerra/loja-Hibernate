4486aaeea4addba9eba6dbd5b4dee143dcce9260
José Bezerra <josbezfi@gmail.com>
2021-09-14 21:50:41 -0300

    Consultas com o find e JPQL.

--- src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
+++ src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -1,32 +1,49 @@
 package br.com.alura.loja.testes;
 
+import br.com.alura.loja.dao.CategoriaDAO;
+import br.com.alura.loja.dao.ProdutoDAO;
 import br.com.alura.loja.modelo.Categoria;
+import br.com.alura.loja.modelo.Produto;
 import br.com.alura.loja.util.JPAUtil;
 
 import javax.persistence.EntityManager;
+import java.math.BigDecimal;
+import java.util.List;
 
 public class CadastroDeProduto {
 
     public static void main(String[] args) {
+        cadastrarProduto();
+        EntityManager em = JPAUtil.getEntityManager();
+        ProdutoDAO produtoDAO = new ProdutoDAO(em);
+        Produto p = produtoDAO.buscarPorId(1l);
+        System.out.println(p.getPreco());
+
+        List<Produto> todosProdutos = produtoDAO.buscarPorNomeDaCategoria("CELULARES");
+        todosProdutos.forEach(p2 -> System.out.println(p.getNome()));
+
+        BigDecimal predoDoProduto = produtoDAO.buscarPrecoDoProdutoComNome("Iphone 11");
+        System.out.println("Preço do Produto: " + predoDoProduto);
+
+
+
+    }
+
+    private static void cadastrarProduto() {
         Categoria celulares = new Categoria("CELULARES");
+        Produto celular = new Produto("Iphone 11", "Red",
+                new BigDecimal("800"),  celulares );
 
         EntityManager em = JPAUtil.getEntityManager();//compartilhado com as DAOS
+        CategoriaDAO categoriaDAO = new CategoriaDAO(em);
+        ProdutoDAO produtoDAO = new ProdutoDAO(em);
 
         em.getTransaction().begin();
 
-        em.persist(celulares);
-        celulares.setNome("Notebooks");
+        categoriaDAO.cadastrar(celulares);
+        produtoDAO.cadastrar(celular);
 
-        em.flush();//sincroniza parcialmente com o banco
-        em.clear();//limpa as entidades gerenciadas
-
-        celulares = em.merge(celulares);//devolve nova referência
-        celulares.setNome("Desktop");
-        em.flush();//continua managed
-        em.clear();
-        em.remove(celulares);
-        em.flush();
-
-
+        em.getTransaction().commit();
+        em.close();
     }
 }
