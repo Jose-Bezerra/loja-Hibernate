a7db08379395a89b371f66c423fa88e8de4750d6
José Bezerra <josbezfi@gmail.com>
2021-09-21 20:31:02 -0300

    Criando as entidades Cliente, Pedido e ItemPedido. Configurado relacionamentos.

---
+++ src/main/java/br/com/alura/loja/modelo/Pedido.java
@@ -0,0 +1,66 @@
+package br.com.alura.loja.modelo;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "pedido")
+public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal valorTotal;// A JPA entende o camelCase como underline no BD
+    private LocalDate data = LocalDate.now();
+    @ManyToOne //relacionamento entre entidades  - muitos pedidos para um cliente
+    private Cliente cliente;
+    @OneToMany(mappedBy = "pedido")
+    private List<ItemPedido> itens = new ArrayList<>(); //boa prática - iniciar a lista
+
+    public Pedido() {
+    }
+
+    public Pedido(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(BigDecimal valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public void adicionarItem(ItemPedido item) {
+        item.setPedido(this); //O item conhece o pedido
+        this.itens.add(item);//O pedido conhece o item (lista)
+    }
+}
